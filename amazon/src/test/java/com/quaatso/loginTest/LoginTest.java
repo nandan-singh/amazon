87aba722526574062360ea7915aafed91a3aae75
nandan <nandan_singh@live.com>
2016-11-03 22:30:22 +0530

    initial commit

---
+++ amazon/src/test/java/com/quaatso/loginTest/LoginTest.java
@@ -0,0 +1,13 @@
+package com.quaatso.loginTest;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	
+	@Test(enabled=false)
+	public void verifyLoginWithValidCredential(){
+		
+	}
+
+}
